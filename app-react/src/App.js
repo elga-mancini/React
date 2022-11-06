e54ebea03512e4b7c3bc044293a5930aa4960e50
Elga <elga.mancini@gmail.com>
2022-11-06 10:32:19 +0100

    eseguito

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -4,7 +4,7 @@ export class App extends React.Component {
     render(){
         return (
             <div>
-                <Welcome name="Elga" />
+                <Welcome />
             </div>
         )
     }
