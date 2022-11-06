348531b0142c55584d3e2d1779d1b178cdfd7a60
Elga <elga.mancini@gmail.com>
2022-11-06 16:32:18 +0100

    eseguito

--- app-react/src/Age.js
+++ app-react/src/Age.js
@@ -1,10 +1,9 @@
 import React from "react";
 
 export class Age extends React.Component {
-    render () {
-        return (
-            <div>{this.props.age && <p>Your age is {this.props.age}</p>}</div>
-            )
-
-    }
+  render() {
+    return (
+      <div>{this.props.age > 18 && this.props.age < 65 && <p>Your age is {this.props.age}</p>}</div>
+    );
+  }
 }
